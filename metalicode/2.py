56b21208651cea1269b8c9fa71250f107469365a
oritn <orit0261>
2021-05-05 09:58:06 +0300

    update

--- metalicode/2.py
+++ metalicode/2.py
@@ -5,3 +5,19 @@ for i in string:
                 i = '0'
         result.append(i)
 print(''.join(result))
+
+str1 = "i am a string with space"
+
+mystr=str1
+print(mystr[:-1].zfill(40)[::-1])
+
+#print(str1.split())
+print('#'.join(mystr))
+
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print(x)
+
+raise Exception("hohfgph")
