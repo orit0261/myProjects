2dd83a74970b0d47edfab4a775e57df2fa4ca299
oritn <orit0261>
2021-05-24 11:06:15 +0300

    update

--- Rafael/tax_rates.py
+++ Rafael/tax_rates.py
@@ -1,6 +1,5 @@
 def calc_tax(sum, tup_tax):
     sum_tax = 0
-    left = sum
     for lower_sum, upper_sum, tax in tup_tax:
         print(lower_sum, upper_sum, tax)
         if sum > upper_sum :
@@ -10,6 +9,8 @@ def calc_tax(sum, tup_tax):
             left = sum - lower_sum
             sum_tax = sum_tax + (left*tax)
     print(sum_tax)
+
+
 
 tup_tax = [(0, 6000, 0), (6000, 8000, 0.1), (8000, 10000, 0.2), (10000, 999999, 0.3)]
 calc_tax(7000, tup_tax)
